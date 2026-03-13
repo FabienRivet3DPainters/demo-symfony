fe8864561ffca12689dba3ec832ded5f093a4974
mickael <chezdijoux@gmail.com>
2026-03-13 19:19:28 +0100

    modif

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,12 +6,13 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard; // À AJOUTER
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+// SUPPRIMEZ cette ligne : use Symfony\Component\Routing\Attribute\Route;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
+    #[AdminDashboard(routePath: '/admin', routeName: 'admin')] // REMPLACEZ #[Route]
     public function index(): Response
     {
         return $this->render('admin/dashboard.html.twig');
