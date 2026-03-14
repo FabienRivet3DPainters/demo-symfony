4ad6a77e7faaf01ccc7c24c7520002b18684a773
mickael <chezdijoux@gmail.com>
2026-03-14 18:46:19 +0100

    FRONT END CHANGE

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
@@ -17,7 +18,9 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $products = $this->productRepository->findAll();
+        // On affiche seulement 4 produits en vedette sur la page d'accueil
+        // Le catalogue complet reste accessible via /products
+        $products = $this->productRepository->findBy([], ['id' => 'DESC'], 4);
 
         return $this->render('home/index.html.twig', [
             'products' => $products,
