c6aecd878271f1fe9f217cbef7571778f018b502
mickael <chezdijoux@gmail.com>
2026-03-16 13:33:02 +0100

    Temp: Disable DB loading in HomeController to diagnose timeout issue

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,11 +17,9 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $products = $this->productRepository->findAll();
-
-        $recommended = !empty($products)
-            ? $this->recommendationService->getRecommendations($products[0])
-            : [];
+        // Temporaire : désactiver la DB pour diagnostiquer
+        $products = []; // $this->productRepository->findAll();
+        $recommended = []; // !empty($products) ? $this->recommendationService->getRecommendations($products[0]) : [];
 
         return $this->render('home/index.html.twig', [
             'products' => $products,
